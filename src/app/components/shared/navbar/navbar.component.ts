cbdf3cb5c769fdf98de033f5568e25037132b0ad
Jia Jun Yang <jyang@baufest.com>
2018-03-15 11:33:13 -0300

    navbar collapse click y busqueda

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { LogeoService } from '../../../services/logeo.service';
 
+declare var $;
 
 @Component({
   selector: 'app-navbar',
@@ -18,10 +19,17 @@ export class NavbarComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    // $(document).ready(function(){  
+      $('.navbar-nav>li>a').on('click', () => {
+        $('.navbar-collapse').collapse('hide');
+      });
+    // }); 
   }
 
   irABusqueda(buscar: string){
     this.router.navigate(['/busqueda',buscar]);
+    $('.navbar-collapse').collapse('hide');
   }
+
 
 }
